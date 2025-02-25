1ea21667ced570b386785c9b649321d1f0aaaee6
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2025-02-25 14:13:11 -0300

    feat: remocao de comentarios

--- algortimos_gulosos/atv01.py
+++ algortimos_gulosos/atv01.py
@@ -1,38 +1,38 @@
 def merge_sort(lista):
-    if len(lista) <= 1: # Confere se a lista já está ordenada
+    if len(lista) <= 1: 
         return lista
 
-    meio = len(lista) // 2 # dividi a lista no meio
+    meio = len(lista) // 2 
 
-    esquerda = merge_sort(lista[:meio]) # ordena a primeira metade
-    direita = merge_sort(lista[meio:]) # ordena a segunda metade
+    esquerda = merge_sort(lista[:meio]) 
+    direita = merge_sort(lista[meio:]) 
 
-    return merge(esquerda, direita) # mescla as duas metades em uma só lista ordenada
+    return merge(esquerda, direita) 
 
-def merge(esquerda, direita): # combina as duas listas ordenadas em uma única lista ordenada.
-    resultado = [] # lista fazia que vai receber os elementos ordenados
-    i = j = 0 # indices para percorrer as duas listas
+def merge(esquerda, direita): 
+    resultado = [] 
+    i = j = 0 
 
-    while i < len(esquerda) and j < len(direita): # Pecorre a lista e compara os elementos  
+    while i < len(esquerda) and j < len(direita):  
         if esquerda[i] < direita[j]:
-            resultado.append(esquerda[i]) # O menor elemento é adicionado à lista resultado.
+            resultado.append(esquerda[i]) 
             i += 1
         else:
             resultado.append(direita[j])
             j += 1
         
-    resultado += esquerda[i:] # Se restaram elementos, são adicionados à lista resultado
+    resultado += esquerda[i:] 
     resultado += direita[j:]
 
     return resultado 
 
-def atividade_gulosa(reunioes): # Ordena a lista de reuniões pelo horário de término
+def atividade_gulosa(reunioes): 
     reunioes = merge_sort(reunioes)
-    reunioes_selecionadas = [reunioes[0]] # Seleciona a primeira reunião
+    reunioes_selecionadas = [reunioes[0]] 
 
-    for i in range(1, len(reunioes)): # Percorre a lista de reuniões ordenadas 
-        if reunioes[i][0] >= reunioes_selecionadas[-1][1]: #Verifica se a próxima reunião começa depois ou exatamente quando a última terminou.
-            reunioes_selecionadas.append(reunioes[i]) # Se sim, adiciona a reunião à lista de reuniões selecionadas
+    for i in range(1, len(reunioes)): 
+        if reunioes[i][0] >= reunioes_selecionadas[-1][1]: 
+            reunioes_selecionadas.append(reunioes[i]) 
     return reunioes_selecionadas
 
 reunioes = [(1, 3), (2, 5), (4, 6), (6, 8), (5, 7)]
